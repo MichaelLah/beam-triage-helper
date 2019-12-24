7bd324fb6f6988fe42d51e82c0ca36512590d00a
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2019-12-23 21:55:16 -0500

    some progress

--- popup.js
+++ popup.js
@@ -1,14 +1,24 @@
-let changeColor = document.getElementById('changeColor');
+let changeColor = document.getElementById("changeColor");
+let adminButton = document.getElementById("admin");
 
-chrome.storage.sync.get('color', function (data) {
+chrome.storage.sync.get("color", function(data) {
   changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+  changeColor.setAttribute("value", data.color);
 });
-changeColor.onclick = function (element) {
+changeColor.onclick = function(element) {
   let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(
-      tabs[0].id,
-      { code: 'document.body.style.backgroundColor = "' + color + '";' });
+  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+    chrome.tabs.executeScript(tabs[0].id, {
+      code: 'document.body.style.backgroundColor = "' + color + '";'
+    });
   });
 };
+adminButton.onclick = () => {
+  chrome.extension.getBackgroundPage().console.log("ES6");
+};
+chrome.extension.getBackgroundPage().console.log("DUDE");
+chrome.extension.getBackgroundPage().console.log(adminButton);
+// adminButton.onClick = function() {
+//   console.log("yeet")
+//   chrome.extension.getBackgroundPage().console.log('foo');
+// }
