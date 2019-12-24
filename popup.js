8fd95a62f3d4b4f6936c5ee0298634cc53f24996
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2019-12-23 23:10:09 -0500

    it works

--- popup.js
+++ popup.js
@@ -1,24 +1,38 @@
 let changeColor = document.getElementById("changeColor");
 let adminButton = document.getElementById("admin");
+// let lighthouseButton = document.getElementById("lighthouse");
 
-chrome.storage.sync.get("color", function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute("value", data.color);
-});
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-    chrome.tabs.executeScript(tabs[0].id, {
-      code: 'document.body.style.backgroundColor = "' + color + '";'
-    });
+// chrome.storage.sync.get("color", function(data) {
+//   changeColor.style.backgroundColor = data.color;
+//   changeColor.setAttribute("value", data.color);
+// });
+// changeColor.onclick = function(element) {
+//   let color = element.target.value;
+//   chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+//     chrome.tabs.executeScript(tabs[0].id, {
+//       code: 'document.body.style.backgroundColor = "' + color + '";'
+//     });
+//   });
+// };
+adminButton.onclick = () => {
+  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
+    const url = tabs[0] && tabs[0].url;
+    chrome.extension.getBackgroundPage().console.log(url);
+    if (url && url.includes("app.beam.dental")) {
+      const localAdmin = url.replace("app.beam.dental", "app.beam.localhost");
+      const noHttp = localAdmin.replace("https://", 'http://')
+      chrome.tabs.update({ url: noHttp });
+    }
   });
 };
-adminButton.onclick = () => {
-  chrome.extension.getBackgroundPage().console.log("ES6");
-};
-chrome.extension.getBackgroundPage().console.log("DUDE");
-chrome.extension.getBackgroundPage().console.log(adminButton);
-// adminButton.onClick = function() {
-//   console.log("yeet")
-//   chrome.extension.getBackgroundPage().console.log('foo');
-// }
+// lighthouseButton.onclick = () => {
+//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
+//     const url = tabs[0] && tabs[0].url;
+//     chrome.extension.getBackgroundPage().console.log(url);
+//     if (url && url.includes("app.beam.dental")) {
+//       const localAdmin = url.replace("app.beam.dental", "app.beam.localhost");
+//       const noHttp = localAdmin.replace("https://", 'http://')
+//       chrome.tabs.update({ url: noHttp });
+//     }
+//   });
+// };
