19577810d0c00278848d8d9628a987bd82442e1e
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-01-03 10:45:19 -0500

    copy resource IDs to clipboard

--- background.js
+++ background.js
@@ -1,16 +1,19 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({ color: "#3aa757" }, function() {
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.sync.set({color: "#3aa757"}, function () {
     console.log("The color is green.");
   });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "lighthouse.beam.dental" }
+            pageUrl: {hostEquals: "lighthouse.beam.dental"}
           }),
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "app.beam.dental" }
+            pageUrl: {hostEquals: "app.beam.dental"}
+          }),
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {hostEquals: "app.beam.localhost"}
           })
         ],
         actions: [new chrome.declarativeContent.ShowPageAction()]
--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 let changeColor = document.getElementById("changeColor");
 let adminButton = document.getElementById("admin");
 // let lighthouseButton = document.getElementById("lighthouse");
+let copyIdButton = document.getElementById("copyId");
 
 // chrome.storage.sync.get("color", function(data) {
 //   changeColor.style.backgroundColor = data.color;
@@ -15,24 +16,39 @@ let adminButton = document.getElementById("admin");
 //   });
 // };
 adminButton.onclick = () => {
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, function(tabs) {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, function (tabs) {
     const url = tabs[0] && tabs[0].url;
-    chrome.extension.getBackgroundPage().console.log(url);
     if (url && url.includes("app.beam.dental")) {
       const localAdmin = url.replace("app.beam.dental", "app.beam.localhost");
       const noHttp = localAdmin.replace("https://", 'http://')
-      chrome.tabs.update({ url: noHttp });
+      chrome.tabs.create({url: noHttp});
     }
   });
 };
-// lighthouseButton.onclick = () => {
-//   chrome.tabs.query({ active: true, lastFocusedWindow: true }, function (tabs) {
-//     const url = tabs[0] && tabs[0].url;
-//     chrome.extension.getBackgroundPage().console.log(url);
-//     if (url && url.includes("app.beam.dental")) {
-//       const localAdmin = url.replace("app.beam.dental", "app.beam.localhost");
-//       const noHttp = localAdmin.replace("https://", 'http://')
-//       chrome.tabs.update({ url: noHttp });
-//     }
-//   });
-// };
+copyIdButton.onclick = () => {
+  chrome.tabs.query({}, tabs => {
+    tabs.forEach(tb => {
+      chrome.extension.getBackgroundPage().console.log('foo');
+    })
+  })
+
+  chrome.tabs.query({active: true}, tabs => {
+    const url = tabs[0] && tabs[0].url;
+    const re = new RegExp('\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}')
+    const tumbledRe = new RegExp('\\w{32}')
+    const idMatches = url.match(re)
+    const tumbledIdMatches = url.match(tumbledRe)
+    const id = idMatches && idMatches.length > 0 ? idMatches[0] : ''
+    const tumbledId = tumbledIdMatches && tumbledIdMatches.length > 0 ? tumbledIdMatches[0] : ''
+    chrome.extension.getBackgroundPage().console.log("matching ID:", id);
+    chrome.extension.getBackgroundPage().console.log("matching tumbled ID:", tumbledId);
+    navigator.clipboard.writeText(id || tumbledId)
+    .then(() => {
+      chrome.extension.getBackgroundPage().console.log('Text copied to clipboard');
+    })
+    .catch(err => {
+      // This can happen if the user denies clipboard permissions:
+      chrome.extension.getBackgroundPage().console.error('Could not copy text: ', err);
+    });
+  });
+};
